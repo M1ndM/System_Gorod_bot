72f779dddda3039dc59f5abacdeec7ac5049e722
user <userer>
2019-12-12 19:01:47 +0700

    сделал потенциал записи в сторонний источник и устранил повторные показания

--- chatbotv2neww.py
+++ chatbotv2neww.py
@@ -9,8 +9,8 @@ opts = {
                   'горячая вода в туалете', 'холодная вода в туалете', 'электричество')
 }
 list_pokazania = ['холодная вода на кухне', 'горячая вода на кухне',
-                  'горячая вода в туалете', 'холодная вода в туалете', 'электричество']
-g = []
+                  'горячая вода в туалете', 'холодная вода в туалете']
+
 
 f = open('list_pokazania.txt', 'w')
 
@@ -23,24 +23,33 @@ def speak(what):
 
 
 def dialogue():
-    speak('tell me pokazania')
+    speak('Здравствуйте, я чат-бот для сбора показаний ЖКХ')
+    speak('Скажите показания в виде' + ' "' + 'холодная вода на кухне, количество' + '"')
     with sr.Microphone(device_index=0) as source:
         audio = r.listen(source)
         pokaz = r.recognize_google(audio, language='ru')
         print(pokaz)
+        for c in list_pokazania:
+            b = fuzz.ratio(c, pokaz)
+            if b > 80:
+                speak('Вы уже говорили это')
         for i in opts["pokazania"]:
             s = fuzz.ratio(i, pokaz)
             if s >= 71:
                 print(i)
-                list_pokazania.append(i)
+                list_pokazania.append(pokaz)
+
+
+def writePokaz():
+    for index in list_pokazania:
+        f.write(index + '\n')
+    f.close()
 
 
 while True:
     if len(list_pokazania) < 5:
         dialogue()
     else:
-        speak('you told me all pokazania, goodbye!')
-        for index in list_pokazania:
-            f.write(index + '\n')
-        f.close()
+        speak('Вы сказали все показания, досвидания!')
+        writePokaz()
         quit()
--- nado.py
+++ nado.py
@@ -1,6 +1,13 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-g = ['dsvsfgvs', 'dfvsgbwevsdfws', 'ghfuhfkjouidshkjsd', 'Привет 9009']
+d = []
+f = input()
+g = ['холодная вода на кухне', 'горячая вода на кухне',
+     'горячая вода в туалете', 'холодная вода в туалете', 'электричество']
 for i in g:
-    s = fuzz.ratio('Привет', i)
-    print(s)
+    s = fuzz.ratio(i, f)
+    if s >= 80:
+        print(i)
+        d.append(i)
+        print(f)
+# 9 нулей максимум
